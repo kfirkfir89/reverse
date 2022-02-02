5f0d6787d0e540b49029f3732df5d2038e507e91
kfir <kfirkfir89@gmail.com>
2022-02-02 15:03:54 +0200

    finished challenge
    added js and all the rest :D

--- js/site.js
+++ js/site.js
@@ -1,3 +1,34 @@
-function helloWorld(){
-    alert("hello world");
+//Get the string fromt he page
+//Controller function
+function getValues(){
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+    displayString(revString);
+
+}
+
+//Reverse the string
+//Logic function
+function reverseString(userString){
+
+    let revString = [];
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
+
+}
+
+//Display the reverse string to the user
+//View Function
+function displayString(revString){
+
+    //wrtie to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`
+    //show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+
 }
